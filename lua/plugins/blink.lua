6c97cad91cc4692ffaacbdd2e1ed88cc41de8857
Chase Counsell <cjcounsell@gmail.com>
2025-04-25 08:10:39 -0400

    add keys to blink.cmp

---
+++ lua/plugins/blink.lua
@@ -0,0 +1,9 @@
+return {
+  "saghen/blink.cmp",
+  opts = {
+    keymap = {
+      ["<C-j>"] = { "select_next", "fallback_to_mappings" },
+      ["<C-k>"] = { "select_prev", "fallback_to_mappings" },
+    },
+  },
+}

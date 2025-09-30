5a464f5fbf9370a411eae993c55ccc8674cbfb60
Chase Counsell <cjcounsell@gmail.com>
2025-09-30 09:25:18 -0400

    automated dev commit

---
+++ lua/plugins/lang/sql.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "mfussenegger/nvim-lint",
+    opts = {
+      linters = {
+        sqlfluff = {
+          args = { "lint", "--format=json", "--dialect=tsql" },
+        },
+      },
+    },
+  },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters = {
+        sqlfluff = {
+          args = { "format", "--dialect=tsql", "-" },
+        },
+      },
+    },
+  },
+}

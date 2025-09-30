7184d548a745d4b7b8626e01a26e34e7e7546928
Chase Counsell <cjcounsell@gmail.com>
2025-09-29 22:41:01 -0400

    automated dev commit

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,7 +1,7 @@
 return {
   "akinsho/bufferline.nvim",
   init = function()
-    local bufferline = require("catppuccin.groups.integrations.bufferline")
+    local bufferline = require("catppuccin.special.bufferline")
     bufferline.get = bufferline.get or bufferline.get_theme
   end,
 }
